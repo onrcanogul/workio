fd6bd5f91e3a87f25a635e2a9ce192a6a3b6910d
onrcanogul <onurcanogul@ogr.eskisehir.edu.tr>
2025-03-09 01:26:50 +0300

    minor bug fixed :)

--- Application/Workio.Application.Abstraction/src/User/Dto/UserDto.cs
+++ Application/Workio.Application.Abstraction/src/User/Dto/UserDto.cs
@@ -1,3 +1,6 @@
+using Workio.Application.Abstraction.Application.Dto;
+using Workio.Application.Abstraction.Job.Dto;
+
 namespace Workio.Application.Abstraction.src.User.Dto;
 
 public class UserDto
@@ -5,4 +8,6 @@ public class UserDto
     public Guid Id { get; set; }
     public string Email { get; set; } = null!;
     public string Username { get; set; } = null!;
+    public List<JobDto> Jobs { get; set; } = [];
+    public List<ApplicationDto> Applications { get; set; } = [];
 }
